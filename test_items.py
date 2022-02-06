ccbf4d34e6221959c242a3af26a237526e504ad2
ShakhovaAnastasiz <38289843+ShakhovaAnastasiz@users.noreply.github.com>
2022-02-06 17:36:51 +0200

    Add conftest.py and test_items.py files
    Add conftest.py with fixture for running auto tests in different language interfaces
    Add test_items.py with test which check if the button "Add to basket" is present on web-page

--- test_items.py
+++ test_items.py
@@ -1,1 +1,13 @@
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+def test_btnAddToCart_isPresent(browser):
+    browser.get(link)
+    btnAddToCart= None
+    try:
+        btnAddToCart = browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
+    except NoSuchElementException:
+        assert btnAddToCart is not None, 'There is no button "Add to Cart" on the page'
 
